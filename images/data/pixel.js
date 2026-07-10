658322739eff587d9de67eab8c8acdd63e6658fc
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 00:47:58 +0500

    Add Google Pixel models database

---
+++ images/data/pixel.js
@@ -0,0 +1,35 @@
+const pixelModels = [
+
+"Pixel 3",
+"Pixel 3 XL",
+"Pixel 3a",
+"Pixel 3a XL",
+
+"Pixel 4",
+"Pixel 4 XL",
+"Pixel 4a",
+"Pixel 4a 5G",
+
+"Pixel 5",
+"Pixel 5a",
+
+"Pixel 6",
+"Pixel 6 Pro",
+"Pixel 6a",
+
+"Pixel 7",
+"Pixel 7 Pro",
+"Pixel 7a",
+
+"Pixel Fold",
+
+"Pixel 8",
+"Pixel 8 Pro",
+"Pixel 8a",
+
+"Pixel 9",
+"Pixel 9 Pro",
+"Pixel 9 Pro XL",
+"Pixel 9 Pro Fold"
+
+];
