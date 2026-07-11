a0c38d0febd09276ed76804f3bcd6e95ed73c21b
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 22:42:46 +0500

    TechFix V6 Apple Database

--- images/data/apple.js
+++ images/data/apple.js
@@ -1,99 +1,57 @@
-const appleModels = [
-
-/* Classic */
-
-"iPhone 2G",
-"iPhone 3G",
-"iPhone 3GS",
-
-/* iPhone 4 */
-
-"iPhone 4",
-"iPhone 4S",
-
-/* iPhone 5 */
-
-"iPhone 5",
-"iPhone 5C",
-"iPhone 5S",
-
-/* SE */
-
-"iPhone SE (1st Gen)",
-
-/* iPhone 6 */
+const apple=[
 
 "iPhone 6",
 "iPhone 6 Plus",
-"iPhone 6S",
-"iPhone 6S Plus",
 
-/* iPhone 7 */
+"iPhone 6s",
+"iPhone 6s Plus",
+
+"iPhone SE (1st Gen)",
 
 "iPhone 7",
 "iPhone 7 Plus",
 
-/* iPhone 8 */
-
 "iPhone 8",
 "iPhone 8 Plus",
 
-/* iPhone X */
-
 "iPhone X",
+
 "iPhone XR",
+
 "iPhone XS",
 "iPhone XS Max",
-  /* iPhone 11 */
 
 "iPhone 11",
 "iPhone 11 Pro",
 "iPhone 11 Pro Max",
 
-/* iPhone 12 */
+"iPhone SE (2nd Gen)",
 
 "iPhone 12 Mini",
 "iPhone 12",
 "iPhone 12 Pro",
 "iPhone 12 Pro Max",
 
-/* iPhone 13 */
-
 "iPhone 13 Mini",
 "iPhone 13",
 "iPhone 13 Pro",
 "iPhone 13 Pro Max",
 
-/* iPhone SE */
-
-"iPhone SE (2nd Gen)",
 "iPhone SE (3rd Gen)",
-
-/* iPhone 14 */
 
 "iPhone 14",
 "iPhone 14 Plus",
 "iPhone 14 Pro",
 "iPhone 14 Pro Max",
 
-/* iPhone 15 */
-
 "iPhone 15",
 "iPhone 15 Plus",
 "iPhone 15 Pro",
 "iPhone 15 Pro Max",
-  /* iPhone 16 */
 
 "iPhone 16",
 "iPhone 16 Plus",
 "iPhone 16 Pro",
-"iPhone 16 Pro Max",
-
-/* iPhone 17 */
-
-"iPhone 17",
-"iPhone 17 Air",
-"iPhone 17 Pro",
-"iPhone 17 Pro Max"
+"iPhone 16 Pro Max"
 
 ];
