c0d79e269911c500afab9dd2bf450ae49ce44cda
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 09:42:33 +0500

    Add complete Huawei models database

---
+++ images/data/huawei.js
@@ -0,0 +1,93 @@
+const huaweiModels = [
+
+/* P Series */
+
+"Huawei P8",
+"Huawei P9",
+"Huawei P9 Lite",
+"Huawei P10",
+"Huawei P10 Plus",
+"Huawei P20",
+"Huawei P20 Lite",
+"Huawei P20 Pro",
+"Huawei P30",
+"Huawei P30 Lite",
+"Huawei P30 Pro",
+"Huawei P40",
+"Huawei P40 Lite",
+"Huawei P40 Pro",
+"Huawei P50",
+"Huawei P50 Pro",
+"Huawei P60",
+"Huawei P60 Pro",
+"Huawei Pura 70",
+"Huawei Pura 70 Pro",
+"Huawei Pura 70 Ultra",
+
+/* Mate Series */
+
+"Huawei Mate 8",
+"Huawei Mate 9",
+"Huawei Mate 10",
+"Huawei Mate 10 Pro",
+"Huawei Mate 20",
+"Huawei Mate 20 Lite",
+"Huawei Mate 20 Pro",
+"Huawei Mate 30",
+"Huawei Mate 30 Pro",
+"Huawei Mate 40",
+"Huawei Mate 40 Pro",
+"Huawei Mate 50",
+"Huawei Mate 50 Pro",
+"Huawei Mate 60",
+"Huawei Mate 60 Pro",
+"Huawei Mate X",
+"Huawei Mate X2",
+"Huawei Mate X3",
+"Huawei Mate X5",
+"Huawei Mate XT",
+
+/* Nova Series */
+
+"Huawei Nova",
+"Huawei Nova 2",
+"Huawei Nova 3",
+"Huawei Nova 4",
+"Huawei Nova 5",
+"Huawei Nova 5T",
+"Huawei Nova 6",
+"Huawei Nova 7",
+"Huawei Nova 8",
+"Huawei Nova 9",
+"Huawei Nova 10",
+"Huawei Nova 11",
+"Huawei Nova 12",
+"Huawei Nova 13",
+
+/* Y Series */
+
+"Huawei Y5",
+"Huawei Y5 II",
+"Huawei Y5 Prime",
+"Huawei Y6",
+"Huawei Y6 Prime",
+"Huawei Y7",
+"Huawei Y7 Prime",
+"Huawei Y9",
+"Huawei Y9 Prime",
+"Huawei Y9a",
+"Huawei Y3",
+"Huawei Y5p",
+"Huawei Y6p",
+"Huawei Y7p",
+"Huawei Y8p",
+
+/* Enjoy Series */
+
+"Huawei Enjoy 10",
+"Huawei Enjoy 20",
+"Huawei Enjoy 50",
+"Huawei Enjoy 60",
+"Huawei Enjoy 70"
+
+];
