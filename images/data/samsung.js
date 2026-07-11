c10e0e8a9a849c2b8e61a43ee08c1a2df4682afa
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 08:52:30 +0500

    Add complete Samsung models database

---
+++ images/data/samsung.js
@@ -0,0 +1,204 @@
+const samsungModels = [
+
+/* Galaxy S Series */
+
+"Galaxy S",
+"Galaxy S II",
+"Galaxy S III",
+"Galaxy S4",
+"Galaxy S5",
+"Galaxy S6",
+"Galaxy S6 Edge",
+"Galaxy S6 Edge+",
+
+"Galaxy S7",
+"Galaxy S7 Edge",
+
+"Galaxy S8",
+"Galaxy S8+",
+
+"Galaxy S9",
+"Galaxy S9+",
+
+"Galaxy S10e",
+"Galaxy S10",
+"Galaxy S10+",
+"Galaxy S10 Lite",
+
+"Galaxy S20",
+"Galaxy S20+",
+"Galaxy S20 Ultra",
+"Galaxy S20 FE",
+  /* Galaxy S21 Series */
+
+"Galaxy S21",
+"Galaxy S21+",
+"Galaxy S21 Ultra",
+"Galaxy S21 FE",
+
+/* Galaxy S22 Series */
+
+"Galaxy S22",
+"Galaxy S22+",
+"Galaxy S22 Ultra",
+
+/* Galaxy S23 Series */
+
+"Galaxy S23",
+"Galaxy S23+",
+"Galaxy S23 Ultra",
+"Galaxy S23 FE",
+
+/* Galaxy S24 Series */
+
+"Galaxy S24",
+"Galaxy S24+",
+"Galaxy S24 Ultra",
+"Galaxy S24 FE",
+
+/* Galaxy S25 Series */
+
+"Galaxy S25",
+"Galaxy S25+",
+"Galaxy S25 Ultra",
+"Galaxy S25 Edge",
+  /* Galaxy Note Series */
+
+"Galaxy Note",
+"Galaxy Note II",
+"Galaxy Note 3",
+"Galaxy Note 4",
+"Galaxy Note 5",
+"Galaxy Note 7",
+"Galaxy Note FE",
+"Galaxy Note 8",
+"Galaxy Note 9",
+"Galaxy Note 10",
+"Galaxy Note 10+",
+"Galaxy Note 10 Lite",
+"Galaxy Note 20",
+"Galaxy Note 20 Ultra",
+
+/* Galaxy Z Fold */
+
+"Galaxy Fold",
+"Galaxy Z Fold2",
+"Galaxy Z Fold3",
+"Galaxy Z Fold4",
+"Galaxy Z Fold5",
+"Galaxy Z Fold6",
+"Galaxy Z Fold7",
+
+/* Galaxy Z Flip */
+
+"Galaxy Z Flip",
+"Galaxy Z Flip 5G",
+"Galaxy Z Flip3",
+"Galaxy Z Flip4",
+"Galaxy Z Flip5",
+"Galaxy Z Flip6",
+"Galaxy Z Flip7",
+  /* Galaxy A Series */
+
+"Galaxy A01",
+"Galaxy A02",
+"Galaxy A03",
+"Galaxy A04",
+"Galaxy A05",
+"Galaxy A06",
+
+"Galaxy A10",
+"Galaxy A11",
+"Galaxy A12",
+"Galaxy A13",
+"Galaxy A14",
+"Galaxy A15",
+"Galaxy A16",
+
+"Galaxy A20",
+"Galaxy A21",
+"Galaxy A22",
+"Galaxy A23",
+"Galaxy A24",
+"Galaxy A25",
+"Galaxy A26",
+
+"Galaxy A30",
+"Galaxy A31",
+"Galaxy A32",
+"Galaxy A33",
+"Galaxy A34",
+"Galaxy A35",
+"Galaxy A36",
+
+"Galaxy A50",
+"Galaxy A51",
+"Galaxy A52",
+"Galaxy A53",
+"Galaxy A54",
+"Galaxy A55",
+"Galaxy A56",
+
+"Galaxy A70",
+"Galaxy A71",
+"Galaxy A72",
+"Galaxy A73",
+
+/* Galaxy M Series */
+
+"Galaxy M10",
+"Galaxy M11",
+"Galaxy M12",
+"Galaxy M13",
+"Galaxy M14",
+"Galaxy M15",
+
+"Galaxy M20",
+"Galaxy M21",
+"Galaxy M22",
+"Galaxy M23",
+"Galaxy M24",
+"Galaxy M25",
+
+"Galaxy M30",
+"Galaxy M31",
+"Galaxy M32",
+"Galaxy M33",
+"Galaxy M34",
+"Galaxy M35",
+
+"Galaxy M40",
+"Galaxy M51",
+"Galaxy M52",
+"Galaxy M53",
+"Galaxy M54",
+"Galaxy M55",
+
+/* Galaxy F Series */
+
+"Galaxy F02",
+"Galaxy F04",
+"Galaxy F05",
+
+"Galaxy F12",
+"Galaxy F13",
+"Galaxy F14",
+"Galaxy F15",
+"Galaxy F16",
+
+"Galaxy F22",
+"Galaxy F23",
+"Galaxy F34",
+"Galaxy F41",
+"Galaxy F42",
+"Galaxy F54",
+"Galaxy F55",
+
+/* XCover Series */
+
+"Galaxy XCover Pro",
+"Galaxy XCover 5",
+"Galaxy XCover 6 Pro",
+"Galaxy XCover 7"
+
+];
