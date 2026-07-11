4ab37ebb60bca23c12178a594ad75e08401a8283
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 09:19:10 +0500

    Add complete OPPO models database

---
+++ images/data/oppo.js
@@ -0,0 +1,173 @@
+const oppoModels = [
+
+/* Find Series */
+
+"OPPO Find 5",
+"OPPO Find 7",
+"OPPO Find 7a",
+
+"OPPO Find X",
+"OPPO Find X Lamborghini",
+"OPPO Find X2",
+"OPPO Find X2 Neo",
+"OPPO Find X2 Lite",
+"OPPO Find X2 Pro",
+
+"OPPO Find X3",
+"OPPO Find X3 Neo",
+"OPPO Find X3 Lite",
+"OPPO Find X3 Pro",
+
+"OPPO Find X5",
+"OPPO Find X5 Lite",
+"OPPO Find X5 Pro",
+
+"OPPO Find X6",
+"OPPO Find X6 Pro",
+
+"OPPO Find X7",
+"OPPO Find X7 Ultra",
+
+"OPPO Find N",
+"OPPO Find N2",
+"OPPO Find N2 Flip",
+"OPPO Find N3",
+"OPPO Find N3 Flip",
+
+/* Reno Series */
+
+"OPPO Reno",
+"OPPO Reno 10x Zoom",
+"OPPO Reno2",
+"OPPO Reno2 F",
+"OPPO Reno2 Z",
+"OPPO Reno3",
+"OPPO Reno3 Pro",
+"OPPO Reno4",
+"OPPO Reno4 Pro",
+"OPPO Reno5",
+"OPPO Reno5 Pro",
+"OPPO Reno6",
+"OPPO Reno6 Pro",
+  /* Reno Series Continued */
+
+"OPPO Reno7",
+"OPPO Reno7 Z",
+"OPPO Reno7 Pro",
+
+"OPPO Reno8",
+"OPPO Reno8 T",
+"OPPO Reno8 Pro",
+
+"OPPO Reno9",
+"OPPO Reno9 Pro",
+"OPPO Reno9 Pro+",
+
+"OPPO Reno10",
+"OPPO Reno10 Pro",
+"OPPO Reno10 Pro+",
+
+"OPPO Reno11",
+"OPPO Reno11 F",
+"OPPO Reno11 Pro",
+
+"OPPO Reno12",
+"OPPO Reno12 F",
+"OPPO Reno12 Pro",
+
+"OPPO Reno13",
+"OPPO Reno13 F",
+"OPPO Reno13 Pro",
+
+/* A Series */
+
+"OPPO A1",
+"OPPO A3",
+"OPPO A5",
+"OPPO A5s",
+"OPPO A7",
+"OPPO A9",
+"OPPO A11",
+"OPPO A12",
+"OPPO A15",
+"OPPO A16",
+"OPPO A17",
+"OPPO A18",
+"OPPO A31",
+"OPPO A33",
+"OPPO A52",
+"OPPO A53",
+"OPPO A54",
+"OPPO A55",
+"OPPO A56",
+"OPPO A57",
+"OPPO A58",
+"OPPO A59",
+"OPPO A60",
+  "OPPO A72",
+"OPPO A73",
+"OPPO A74",
+"OPPO A76",
+"OPPO A77",
+"OPPO A78",
+"OPPO A79",
+
+"OPPO A92",
+"OPPO A93",
+"OPPO A94",
+"OPPO A95",
+"OPPO A96",
+"OPPO A97",
+"OPPO A98",
+
+/* F Series */
+
+"OPPO F1",
+"OPPO F1 Plus",
+"OPPO F1s",
+"OPPO F3",
+"OPPO F3 Plus",
+"OPPO F5",
+"OPPO F7",
+"OPPO F9",
+"OPPO F9 Pro",
+"OPPO F11",
+"OPPO F11 Pro",
+"OPPO F15",
+"OPPO F17",
+"OPPO F17 Pro",
+"OPPO F19",
+"OPPO F19 Pro",
+"OPPO F21 Pro",
+"OPPO F23",
+
+/* K Series */
+
+"OPPO K1",
+"OPPO K3",
+"OPPO K5",
+"OPPO K7",
+"OPPO K9",
+"OPPO K9 Pro",
+"OPPO K10",
+"OPPO K10 Pro",
+"OPPO K11",
+  "OPPO K11x",
+"OPPO K12",
+"OPPO K12x",
+
+/* N Series */
+
+"OPPO N1",
+"OPPO N1 Mini",
+"OPPO N3",
+
+/* Foldables */
+
+"OPPO Find N",
+"OPPO Find N2",
+"OPPO Find N2 Flip",
+"OPPO Find N3",
+"OPPO Find N3 Flip"
+
+];
