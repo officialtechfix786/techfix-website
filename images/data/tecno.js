d24848c4e3f945f8c9386e92d77880a68d1d8c49
officialtechfix786 <mianprince7236@gmail.com>
2026-07-11 09:26:46 +0500

    Create tecno.js

---
+++ images/data/tecno.js
@@ -0,0 +1,97 @@
+const tecnoModels = [
+
+/* Phantom Series */
+
+"Tecno Phantom 6",
+"Tecno Phantom 6 Plus",
+"Tecno Phantom 8",
+"Tecno Phantom 9",
+"Tecno Phantom X",
+"Tecno Phantom X2",
+"Tecno Phantom X2 Pro",
+"Tecno Phantom V Fold",
+"Tecno Phantom V Flip",
+"Tecno Phantom Ultimate",
+
+/* Camon Series */
+
+"Tecno Camon 11",
+"Tecno Camon 11 Pro",
+"Tecno Camon 12",
+"Tecno Camon 12 Air",
+"Tecno Camon 12 Pro",
+"Tecno Camon 15",
+"Tecno Camon 15 Air",
+"Tecno Camon 15 Premier",
+"Tecno Camon 16",
+"Tecno Camon 16 Premier",
+"Tecno Camon 17",
+"Tecno Camon 17 Pro",
+"Tecno Camon 18",
+"Tecno Camon 18 Premier",
+"Tecno Camon 19",
+"Tecno Camon 19 Neo",
+"Tecno Camon 19 Pro",
+"Tecno Camon 20",
+"Tecno Camon 20 Pro",
+"Tecno Camon 20 Premier",
+"Tecno Camon 30",
+"Tecno Camon 30 5G",
+"Tecno Camon 30 Pro",
+"Tecno Camon 30 Premier",
+"Tecno Camon 40",
+"Tecno Camon 40 Pro",
+"Tecno Camon 40 Premier",
+
+/* Spark Series */
+
+"Tecno Spark Go",
+"Tecno Spark Go 2020",
+"Tecno Spark Go 2021",
+"Tecno Spark Go 2022",
+"Tecno Spark Go 2023",
+"Tecno Spark Go 2024",
+"Tecno Spark 5",
+"Tecno Spark 5 Pro",
+"Tecno Spark 6",
+"Tecno Spark 6 Go",
+"Tecno Spark 7",
+"Tecno Spark 7 Pro",
+"Tecno Spark 8",
+"Tecno Spark 8 Pro",
+"Tecno Spark 9",
+"Tecno Spark 9 Pro",
+"Tecno Spark 10",
+"Tecno Spark 10 Pro",
+"Tecno Spark 20",
+"Tecno Spark 20 Pro",
+"Tecno Spark 20 Pro+",
+"Tecno Spark 30",
+"Tecno Spark 30 Pro",
+
+/* Pova Series */
+
+"Tecno Pova",
+"Tecno Pova 2",
+"Tecno Pova 3",
+"Tecno Pova 4",
+"Tecno Pova 4 Pro",
+"Tecno Pova 5",
+"Tecno Pova 5 Pro",
+"Tecno Pova 6",
+"Tecno Pova 6 Neo",
+"Tecno Pova 6 Pro",
+
+/* Pop Series */
+
+"Tecno Pop 2",
+"Tecno Pop 3",
+"Tecno Pop 4",
+"Tecno Pop 5",
+"Tecno Pop 5 Go",
+"Tecno Pop 6",
+"Tecno Pop 7",
+"Tecno Pop 8",
+"Tecno Pop 9"
+
+];
