6697a2b9f2516541e4df73f947c6a43beb2d59d4
officialtechfix786 <mianprince7236@gmail.com>
2026-07-14 20:05:28 +0500

    Update database.js

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,13 +1,13 @@
-/*═══════════════════════════════════════
-        TECHFIX DATABASE v1
-        Founder : MIAN AHMAD
-═══════════════════════════════════════*/
-
 "use strict";
 
-const mobileDatabase={
+/*=========================================
+        TECHFIX DATABASE v2
+        Founder : MIAN AHMAD
+=========================================*/
 
-apple:[
+const mobileDatabase = {
+
+apple: [
 
 "iPhone 6",
 "iPhone 6 Plus",
@@ -52,304 +52,6 @@ apple:[
 
 samsung:[
 
-"Galaxy A04",
-"Galaxy A05",
-"Galaxy A05s",
-"Galaxy A06",
-"Galaxy A14",
-"Galaxy A15",
-"Galaxy A16",
-"Galaxy A24",
-"Galaxy A25",
-"Galaxy A34",
-"Galaxy A35",
-"Galaxy A54",
-"Galaxy A55",
-"Galaxy M14",
-"Galaxy M15",
-"Galaxy M34",
-"Galaxy S21",
-"Galaxy S22",
-"Galaxy S23",
-"Galaxy S24",
-"Galaxy S24 Ultra",
-"Galaxy Z Fold 5",
-"Galaxy Z Flip 5"
-
-],
-
-xiaomi:[
-
-"Redmi 10",
-"Redmi 12",
-"Redmi 13",
-"Redmi Note 11",
-"Redmi Note 12",
-"Redmi Note 13",
-"POCO X5",
-"POCO X6",
-"POCO F5",
-"POCO F6",
-"Xiaomi 12",
-"Xiaomi 13",
-"Xiaomi 14"
-
-],
-
-oppo:[
-
-"A17",
-"A18",
-"A38",
-"A58",
-"Reno 8",
-"Reno 10",
-"Reno 11",
-"Find X5",
-"Find X6"
-
-],
-
-vivo:[
-
-"Y17",
-"Y20",
-"Y22",
-"Y27",
-"Y36",
-"V27",
-"V29",
-"V30",
-"X90"
-
-]
-
-};
-/*═══════════════════════════════════════
-        DATABASE PART 2
-═══════════════════════════════════════*/
-
-realme:[
-
-"C11",
-"C21",
-"C25",
-"C30",
-"C33",
-"C35",
-"C51",
-"C53",
-"C55",
-"C61",
-"C63",
-"Narzo 30",
-"Narzo 50",
-"Narzo 60",
-"Narzo 70",
-"Narzo N53",
-"Narzo N55",
-"Realme 8",
-"Realme 9",
-"Realme 10",
-"Realme 11",
-"Realme 12",
-"GT Neo 3",
-"GT Neo 5",
-"GT 6"
-
-],
-
-oneplus:[
-
-"OnePlus 7",
-"OnePlus 7 Pro",
-"OnePlus 8",
-"OnePlus 8 Pro",
-"OnePlus 9",
-"OnePlus 9 Pro",
-"OnePlus 10 Pro",
-"OnePlus 11",
-"OnePlus 12",
-"OnePlus Nord",
-"OnePlus Nord CE",
-"OnePlus Nord CE 2",
-"OnePlus Nord CE 3",
-"OnePlus Nord 2",
-"OnePlus Nord 3"
-
-],
-
-google:[
-
-"Pixel 5",
-"Pixel 5a",
-"Pixel 6",
-"Pixel 6 Pro",
-"Pixel 6a",
-"Pixel 7",
-"Pixel 7 Pro",
-"Pixel 7a",
-"Pixel 8",
-"Pixel 8 Pro",
-"Pixel 8a",
-"Pixel 9",
-"Pixel 9 Pro",
-"Pixel Fold"
-
-],
-
-motorola:[
-
-"Moto G32",
-"Moto G42",
-"Moto G52",
-"Moto G54",
-"Moto G64",
-"Moto G73",
-"Moto G84",
-"Edge 30",
-"Edge 40",
-"Edge 50",
-"Razr 40"
-
-],
-
-huawei:[
-
-"P30",
-"P40",
-"P50",
-"P60",
-"Mate 20",
-"Mate 30",
-"Mate 40",
-"Mate 50",
-"Mate 60",
-"Nova 7",
-"Nova 8",
-"Nova 9",
-"Nova 10"
-
-],
-
-honor:[
-
-"Honor 50",
-"Honor 70",
-"Honor 90",
-"Honor 100",
-"Honor 200",
-"Magic 4",
-"Magic 5",
-"Magic 6",
-"X5",
-"X6",
-"X7",
-"X8",
-"X9"
-
-],
-
-nokia:[
-
-"C10",
-"C20",
-"C21",
-"C30",
-"G10",
-"G20",
-"G21",
-"G22",
-"G42",
-"X10",
-"X20",
-"XR20"
-
-],
-
-tecno:[
-
-"Spark 8",
-"Spark 9",
-"Spark 10",
-"Spark Go",
-"Spark 20",
-"Camon 18",
-"Camon 19",
-"Camon 20",
-"Camon 30",
-"Pova 4",
-"Pova 5",
-"Phantom X2"
-
-],
-
-infinix:[
-
-"Hot 10",
-"Hot 11",
-"Hot 12",
-"Hot 20",
-"Hot 30",
-"Hot 40",
-"Note 10",
-"Note 11",
-"Note 12",
-"Note 30",
-"Zero 20",
-"Zero 30"
-
-],
-
-nothing:[
-
-"Phone (1)",
-"Phone (2)",
-"Phone (2a)",
-"Phone (3a)"
-
-],
-
-sony:[
-
-"Xperia 1 III",
-"Xperia 1 IV",
-"Xperia 1 V",
-"Xperia 5 III",
-"Xperia 5 IV",
-"Xperia 10 V"
-
-],
-
-lg:[
-
-"LG Velvet",
-"LG Wing",
-"LG V60",
-"LG G8",
-"LG K92"
-
-],
-
-asus:[
-
-"ROG Phone 5",
-"ROG Phone 6",
-"ROG Phone 7",
-"Zenfone 8",
-"Zenfone 9",
-"Zenfone 10"
-
-]
-/*═══════════════════════════════════════
-        DATABASE PART 3
-        SAMSUNG COMPLETE
-═══════════════════════════════════════*/
-
-samsung:[
-
-/* Galaxy A Series */
-
 "Galaxy A01",
 "Galaxy A02",
 "Galaxy A02s",
@@ -385,11 +87,7 @@ samsung:[
 "Galaxy A33",
 "Galaxy A34",
 "Galaxy A35",
-"Galaxy A40",
-"Galaxy A41",
-"Galaxy A42",
 "Galaxy A50",
-"Galaxy A50s",
 "Galaxy A51",
 "Galaxy A52",
 "Galaxy A52s",
@@ -400,117 +98,27 @@ samsung:[
 "Galaxy A71",
 "Galaxy A72",
 "Galaxy A73",
-
-/* Galaxy M Series */
-
-"Galaxy M01",
-"Galaxy M02",
-"Galaxy M02s",
-"Galaxy M03",
-"Galaxy M04",
-"Galaxy M10",
-"Galaxy M11",
-"Galaxy M12",
-"Galaxy M13",
-"Galaxy M14",
-"Galaxy M15",
-"Galaxy M20",
-"Galaxy M21",
-"Galaxy M22",
-"Galaxy M23",
-"Galaxy M30",
-"Galaxy M31",
-"Galaxy M32",
-"Galaxy M33",
-"Galaxy M34",
-"Galaxy M35",
-"Galaxy M40",
-"Galaxy M51",
-"Galaxy M52",
-"Galaxy M53",
-"Galaxy M54",
-"Galaxy M55",
-
-/* Galaxy F Series */
-
-"Galaxy F02",
-"Galaxy F04",
-"Galaxy F12",
-"Galaxy F13",
-"Galaxy F14",
-"Galaxy F15",
-"Galaxy F22",
-"Galaxy F23",
-"Galaxy F34",
-"Galaxy F41",
-"Galaxy F42",
-"Galaxy F54",
-
-/* Galaxy S Series */
-
-"Galaxy S8",
-"Galaxy S8+",
-"Galaxy S9",
-"Galaxy S9+",
-"Galaxy S10",
-"Galaxy S10+",
-"Galaxy S10 Lite",
 "Galaxy S20",
-"Galaxy S20+",
-"Galaxy S20 Ultra",
 "Galaxy S20 FE",
 "Galaxy S21",
-"Galaxy S21+",
-"Galaxy S21 Ultra",
 "Galaxy S21 FE",
 "Galaxy S22",
-"Galaxy S22+",
 "Galaxy S22 Ultra",
 "Galaxy S23",
-"Galaxy S23+",
 "Galaxy S23 Ultra",
-"Galaxy S23 FE",
 "Galaxy S24",
-"Galaxy S24+",
 "Galaxy S24 Ultra",
 "Galaxy S25",
-"Galaxy S25+",
 "Galaxy S25 Ultra",
-
-/* Galaxy Note */
-
-"Galaxy Note 8",
-"Galaxy Note 9",
-"Galaxy Note 10",
-"Galaxy Note 10+",
-"Galaxy Note 20",
-"Galaxy Note 20 Ultra",
-
-/* Fold */
-
-"Galaxy Z Fold2",
-"Galaxy Z Fold3",
 "Galaxy Z Fold4",
 "Galaxy Z Fold5",
 "Galaxy Z Fold6",
-
-/* Flip */
-
-"Galaxy Z Flip",
-"Galaxy Z Flip3",
 "Galaxy Z Flip4",
 "Galaxy Z Flip5",
 "Galaxy Z Flip6"
 
 ],
- /*═══════════════════════════════════════
-        DATABASE PART 5
-        XIAOMI FAMILY
-═══════════════════════════════════════*/
-
-xiaomi:[
-
-/* Redmi Number Series */
+        xiaomi:[
 
 "Redmi 7",
 "Redmi 7A",
@@ -528,141 +136,109 @@ xiaomi:[
 "Redmi 13",
 "Redmi 13C",
 "Redmi 14C",
-
-/* Redmi Note Series */
-
-"Redmi Note 7",
-"Redmi Note 7 Pro",
 "Redmi Note 8",
-"Redmi Note 8 Pro",
 "Redmi Note 9",
-"Redmi Note 9S",
-"Redmi Note 9 Pro",
 "Redmi Note 10",
-"Redmi Note 10S",
 "Redmi Note 10 Pro",
-"Redmi Note 10 Pro Max",
 "Redmi Note 11",
-"Redmi Note 11S",
 "Redmi Note 11 Pro",
-"Redmi Note 11 Pro+",
 "Redmi Note 12",
-"Redmi Note 12S",
 "Redmi Note 12 Pro",
-"Redmi Note 12 Pro+",
 "Redmi Note 13",
 "Redmi Note 13 Pro",
-"Redmi Note 13 Pro+",
 "Redmi Note 14",
 "Redmi Note 14 Pro",
-"Redmi Note 14 Pro+",
-
-/* Redmi K Series */
-
-"Redmi K20",
-"Redmi K20 Pro",
-"Redmi K30",
-"Redmi K30 Pro",
-"Redmi K40",
-"Redmi K40 Pro",
-"Redmi K50",
-"Redmi K50 Pro",
-"Redmi K60",
-"Redmi K60 Pro",
-"Redmi K70",
-"Redmi K70 Pro",
-
-/* Xiaomi Number Series */
-
-"Xiaomi 11",
-"Xiaomi 11 Lite",
-"Xiaomi 11T",
-"Xiaomi 11T Pro",
-"Xiaomi 12",
-"Xiaomi 12 Lite",
-"Xiaomi 12 Pro",
-"Xiaomi 12T",
-"Xiaomi 12T Pro",
-"Xiaomi 13",
-"Xiaomi 13 Lite",
-"Xiaomi 13 Pro",
-"Xiaomi 13 Ultra",
-"Xiaomi 14",
-"Xiaomi 14 Pro",
-"Xiaomi 14 Ultra",
-"Xiaomi 15",
-"Xiaomi 15 Pro",
-"Xiaomi 15 Ultra",
-
-/* POCO C Series */
-
-"POCO C31",
-"POCO C40",
-"POCO C50",
 "POCO C51",
 "POCO C55",
 "POCO C61",
 "POCO C65",
-"POCO C75",
-
-/* POCO M Series */
-
-"POCO M2",
 "POCO M3",
 "POCO M4 Pro",
 "POCO M5",
-"POCO M5s",
 "POCO M6",
 "POCO M6 Pro",
-"POCO M7 Pro",
-
-/* POCO X Series */
-
 "POCO X3",
-"POCO X3 NFC",
 "POCO X3 Pro",
 "POCO X4 Pro",
 "POCO X5",
 "POCO X5 Pro",
 "POCO X6",
 "POCO X6 Pro",
-"POCO X7",
-"POCO X7 Pro",
-
-/* POCO F Series */
-
-"POCO F2 Pro",
 "POCO F3",
 "POCO F4",
 "POCO F5",
 "POCO F5 Pro",
 "POCO F6",
-"POCO F6 Pro"
+"POCO F6 Pro",
+"Xiaomi 11",
+"Xiaomi 11T",
+"Xiaomi 12",
+"Xiaomi 12 Pro",
+"Xiaomi 13",
+"Xiaomi 13 Pro",
+"Xiaomi 14",
+"Xiaomi 14 Pro",
+"Xiaomi 15",
+"Xiaomi 15 Pro"
 
 ],
- /*═══════════════════════════════════════
-        DATABASE PART 6
-        OPPO + VIVO + iQOO
-═══════════════════════════════════════*/
+
+realme:[
+
+"C11",
+"C21",
+"C25",
+"C30",
+"C33",
+"C35",
+"C51",
+"C53",
+"C55",
+"C61",
+"C63",
+"Narzo 30",
+"Narzo 50",
+"Narzo 60",
+"Narzo 70",
+"Narzo N53",
+"Narzo N55",
+"Realme 8",
+"Realme 9",
+"Realme 10",
+"Realme 11",
+"Realme 12",
+"GT Neo 3",
+"GT Neo 5",
+"GT 6"
+
+],
+        oneplus:[
+
+"OnePlus 7",
+"OnePlus 7 Pro",
+"OnePlus 8",
+"OnePlus 8 Pro",
+"OnePlus 9",
+"OnePlus 9 Pro",
+"OnePlus 10 Pro",
+"OnePlus 11",
+"OnePlus 12",
+"OnePlus 13",
+"OnePlus Nord",
+"OnePlus Nord CE",
+"OnePlus Nord CE 2",
+"OnePlus Nord CE 3",
+"OnePlus Nord 2",
+"OnePlus Nord 3",
+"OnePlus Nord 4"
+
+],
 
 oppo:[
 
-/* Oppo A Series */
-
-"A1",
-"A3",
-"A5",
-"A5s",
-"A7",
-"A9",
-"A11",
-"A12",
 "A15",
-"A15s",
 "A16",
-"A16k",
 "A17",
-"A17k",
 "A18",
 "A31",
 "A38",
@@ -678,25 +254,15 @@ oppo:[
 "A77",
 "A78",
 "A79",
-"A92",
 "A94",
 "A95",
 "A96",
 "A98",
-
-/* Reno Series */
-
-"Reno 4",
-"Reno 4 Pro",
 "Reno 5",
-"Reno 5 Pro",
 "Reno 6",
-"Reno 6 Pro",
 "Reno 7",
-"Reno 7 Pro",
 "Reno 8",
 "Reno 8 Pro",
-"Reno 9",
 "Reno 10",
 "Reno 10 Pro",
 "Reno 11",
@@ -705,11 +271,6 @@ oppo:[
 "Reno 12 Pro",
 "Reno 13",
 "Reno 13 Pro",
-
-/* Find X */
-
-"Find X2",
-"Find X3",
 "Find X5",
 "Find X6",
 "Find X7",
@@ -718,8 +279,6 @@ oppo:[
 ],
 
 vivo:[
-
-/* Vivo Y Series */
 
 "Y01",
 "Y02",
@@ -742,16 +301,11 @@ vivo:[
 "Y35",
 "Y36",
 "Y38",
-"Y50",
-"Y53",
 "Y55",
 "Y56",
 "Y73",
 "Y75",
 "Y100",
-
-/* Vivo V Series */
-
 "V20",
 "V21",
 "V23",
@@ -760,107 +314,15 @@ vivo:[
 "V29",
 "V30",
 "V40",
-
-/* Vivo X Series */
-
 "X60",
 "X70",
 "X80",
 "X90",
 "X100",
-"X200",
-
-/* Vivo T Series */
-
-"T1",
-"T2",
-"T3"
+"X200"
 
 ],
-
-iqoo:[
-
-"iQOO 7",
-"iQOO 8",
-"iQOO 9",
-"iQOO 10",
-"iQOO 11",
-"iQOO 12",
-"iQOO Neo 6",
-"iQOO Neo 7",
-"iQOO Neo 9",
-"iQOO Z6",
-"iQOO Z7",
-"iQOO Z9"
-
-],
- /*═══════════════════════════════════════
-        DATABASE PART 7
-  APPLE + GOOGLE + MOTOROLA
-═══════════════════════════════════════*/
-
-apple:[
-
-/* iPhone */
-
-"iPhone 6",
-"iPhone 6 Plus",
-"iPhone 6s",
-"iPhone 6s Plus",
-"iPhone SE (1st Gen)",
-"iPhone 7",
-"iPhone 7 Plus",
-"iPhone 8",
-"iPhone 8 Plus",
-"iPhone X",
-"iPhone XR",
-"iPhone XS",
-"iPhone XS Max",
-"iPhone 11",
-"iPhone 11 Pro",
-"iPhone 11 Pro Max",
-"iPhone SE (2020)",
-"iPhone 12",
-"iPhone 12 Mini",
-"iPhone 12 Pro",
-"iPhone 12 Pro Max",
-"iPhone 13",
-"iPhone 13 Mini",
-"iPhone 13 Pro",
-"iPhone 13 Pro Max",
-"iPhone SE (2022)",
-"iPhone 14",
-"iPhone 14 Plus",
-"iPhone 14 Pro",
-"iPhone 14 Pro Max",
-"iPhone 15",
-"iPhone 15 Plus",
-"iPhone 15 Pro",
-"iPhone 15 Pro Max",
-"iPhone 16",
-"iPhone 16 Plus",
-"iPhone 16 Pro",
-"iPhone 16 Pro Max",
-
-/* iPad */
-
-"iPad 6",
-"iPad 7",
-"iPad 8",
-"iPad 9",
-"iPad 10",
-"iPad Air 3",
-"iPad Air 4",
-"iPad Air 5",
-"iPad Air 6",
-"iPad Mini 5",
-"iPad Mini 6",
-"iPad Pro 11",
-"iPad Pro 12.9"
-
-],
-
-google:[
+        google:[
 
 "Pixel 4",
 "Pixel 4 XL",
@@ -873,14 +335,13 @@ google:[
 "Pixel 7",
 "Pixel 7 Pro",
 "Pixel 7a",
-"Pixel Fold",
-"Pixel Tablet",
 "Pixel 8",
 "Pixel 8 Pro",
 "Pixel 8a",
 "Pixel 9",
 "Pixel 9 Pro",
-"Pixel 9 Pro XL"
+"Pixel 9 Pro XL",
+"Pixel Fold"
 
 ],
 
@@ -910,10 +371,6 @@ motorola:[
 "Razr 50"
 
 ],
- /*═══════════════════════════════════════
-        DATABASE PART 8
-     FINAL BRANDS
-═══════════════════════════════════════*/
 
 tecno:[
 
@@ -942,8 +399,7 @@ tecno:[
 "Phantom V Fold"
 
 ],
-
-infinix:[
+        infinix:[
 
 "Hot 10",
 "Hot 11",
@@ -1025,8 +481,7 @@ nokia:[
 "XR20"
 
 ],
-
-sony:[
+        sony:[
 
 "Xperia 1 III",
 "Xperia 1 IV",
@@ -1061,6 +516,33 @@ lg:[
 
 ],
 
+nothing:[
+
+"Phone (1)",
+"Phone (2)",
+"Phone (2a)",
+"Phone (3)",
+"Phone (3a)"
+
+],
+
+iqoo:[
+
+"iQOO 7",
+"iQOO 8",
+"iQOO 9",
+"iQOO 10",
+"iQOO 11",
+"iQOO 12",
+"Neo 6",
+"Neo 7",
+"Neo 9",
+"Z6",
+"Z7",
+"Z9"
+
+],
+
 zte:[
 
 "Blade A31",
@@ -1089,3 +571,5 @@ meizu:[
 "Meizu 21"
 
 ]
+
+};
