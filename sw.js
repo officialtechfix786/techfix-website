bfdb9bdc218df922bbec9e3c68d37e06b90bec0d
officialtechfix786 <mianprince7236@gmail.com>
2026-07-21 18:39:51 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-/* TechFix GitHub Pages service worker. */
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
