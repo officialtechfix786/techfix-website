73dddc203b202885ab5a03635706b1b0bafed25a
officialtechfix786 <mianprince7236@gmail.com>
2026-07-24 22:17:35 +0500

    Create brands.js

---
+++ assets/js/brands.js
@@ -0,0 +1,108 @@
+/*==================================================
+TECHFIX SOFTWARE EXP
+BRANDS DATABASE
+==================================================*/
+
+"use strict";
+
+const brandsDatabase = [
+
+    {
+        id:1,
+        name:"Samsung",
+        slug:"samsung",
+        logo:"assets/images/brands/samsung.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:2,
+        name:"Xiaomi",
+        slug:"xiaomi",
+        logo:"assets/images/brands/xiaomi.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:3,
+        name:"Redmi",
+        slug:"redmi",
+        logo:"assets/images/brands/redmi.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:4,
+        name:"POCO",
+        slug:"poco",
+        logo:"assets/images/brands/poco.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:5,
+        name:"Vivo",
+        slug:"vivo",
+        logo:"assets/images/brands/vivo.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:6,
+        name:"OPPO",
+        slug:"oppo",
+        logo:"assets/images/brands/oppo.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:7,
+        name:"Realme",
+        slug:"realme",
+        logo:"assets/images/brands/realme.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:8,
+        name:"Huawei",
+        slug:"huawei",
+        logo:"assets/images/brands/huawei.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:9,
+        name:"Infinix",
+        slug:"infinix",
+        logo:"assets/images/brands/infinix.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:10,
+        name:"Tecno",
+        slug:"tecno",
+        logo:"assets/images/brands/tecno.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:11,
+        name:"Nokia",
+        slug:"nokia",
+        logo:"assets/images/brands/nokia.png",
+        page:"mobiles.html"
+    },
+
+    {
+        id:12,
+        name:"Motorola",
+        slug:"motorola",
+        logo:"assets/images/brands/motorola.png",
+        page:"mobiles.html"
+    }
+
+];
+
+console.log("Brands Database Loaded");
